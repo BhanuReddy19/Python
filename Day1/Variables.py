1e44461ef6981a8013249d5e5d4c1ac81ad1799e
Bhanu Prakash Reddy <bhanu@example.com>
2025-11-12 19:08:21 +0530

    Python variables

---
+++ Day1/Variables.py
@@ -0,0 +1,17 @@
+"""
+{name_ of_ variable}:{data_type} = {Value}
+age : int =24
+salary: float = 50000
+"""
+age:int=24
+print("Age :",age)
+salary:float = 50000
+print("Salary :",salary)
+name:str="Reddy"
+print("Name :",name)
+Is:bool =True
+print("Statement :",Is)
+Undefined_value="new value"
+print("Undefined_value :",Undefined_value)
+defined_value="old value"
+print("defined_value :",defined_value)
