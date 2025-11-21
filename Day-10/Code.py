fef69e4226803d00fe4371c24bec8ec4d1160fc2
Bhanu Prakash Reddy <bhanu@example.com>
2025-11-21 15:56:03 +0530

    Operators(Assignment and RTelational) & Conditional Statements (if and if-else)

---
+++ Day-10/Code.py
@@ -0,0 +1,98 @@
+## Programs for Assignment Operators
+D = 24
+B = 24
+D +=B
+print("D +=B :", D)
+
+D = 24
+B = 24
+D -=B
+print("D -=B :", D)
+
+D = 24
+B = 24
+D *=B
+print("D *=B :", D)
+
+D = 24
+B = 24
+D **=B
+print("D **=B :", D)
+
+D = 24
+B = 24
+D /=B
+print("D /=B :", D)
+
+D = 24
+B = 24
+D //=B
+print("D //=B :", D)
+
+D = 24
+B = 24
+D %=B
+print("D %=B :", D)
+
+D = 24
+B = 24
+print("D == B", (B == D))
+print("D == B", (D == B))
+print("D != B", (D != B))
+print("D > B", (D > B))
+print("D < B", (D < B))
+print("D >= B", (D >= B))
+print("D <= B", (D <= B))
+
+## Condtional
+age = 24
+
+if age >= 18:
+    print("Eligible for Voting.")
+
+    temp = 40
+
+if temp > 35:
+    print("It's very hot today!")
+
+marks = 45
+
+if marks >= 35:
+    print("Pass")
+
+n = 6
+
+if n % 2 == 0:
+    print("Even Number")
+    
+
+if age >= 17:
+    print("Eligible for Voting.")
+else:
+    print("Not eligible for Voting.")
+
+num = 10   
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+    age = 16
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+marks = 30
+
+if marks >= 35:
+    print("Pass")
+else:
+    print("Fail")
+
+
+
+
